821b7fd6035659e6663cee777dc97880161c976f
mateusz <mateusz@COMMANDERLAMP>
2017-12-06 17:28:31 +0100

    current demo, plotComparison needs reworking as it is for 5days predicting 1

--- ELM_demo2.m
+++ ELM_demo2.m
@@ -1,10 +1,11 @@
 X=ParseCSV('unixdates.csv');
-X1=X;
-[X,Y] = rearrangingData6feat(X);
-[Xtest,Ytest,Xtrain,Ytrain] = setProportionsOfData(X,Y,80);
-elm1 = ELM_Class(5,30,1,'linear',1);
-[oX1,oY1]=rearrangeData(elm1,X1);
-elm = ELM_Class(30,500,6,'linear',1);
+%removing timestamp and volume (we can rmv timestamp cuz we know its each
+%consecutive day)
+X=X(:,2:end-1);
+elm = ELM_Class(30,30,10,size(X,2),'linear',1);
+[oX,oY]=rearrangeData(elm,X);
+[Xtest,Ytest,Xtrain,Ytrain] = setProportionsOfData(oX,oY,80);
+
 disp('training time:')
 tic
 trainedelm = train(elm,Xtrain,Ytrain);
@@ -12,8 +13,8 @@ toc
 pred = predict(trainedelm,Xtest);
 %removing unixtimestamps and volumes as its not neededm we are interested
 %with prices!
-pred=pred(:,2:end-1);
-Ytest=Ytest(:,2:end-1);
+% pred=pred(:,2:end-1);
+% Ytest=Ytest(:,2:end-1);
 disp('R2 error');
 ComputeR2(Ytest,pred)
 plotComparison(pred,Ytest)
