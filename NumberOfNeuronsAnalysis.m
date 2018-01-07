145b8ae63f3d606538d0d1fd3226886a0602ccc3
mateusz <mateusz@COMMANDERLAMP>
2018-01-07 16:37:04 +0100

    Number of Neurons analysis script working now!

--- NumberOfNeuronsAnalysis.m
+++ NumberOfNeuronsAnalysis.m
@@ -1,30 +1,47 @@
 % %% sensitivity analysis on number of hidden neurons
 X=ParseCSV('unixdates.csv');
-[X,Y] = rearrangingData6feat(X);
-[Xtest,Ytest,Xtrain,Ytrain] = setProportionsOfData(X,Y,80);
+%[X,Y] = rearrangingData6feat(X);
+%[Xtest,Ytest,Xtrain,Ytrain] = setProportionsOfData(X,Y,80);
 
-actFun='sig';
+%truncating X dates and volume not needed
+X=X(:,2:end-1);
+
+actFun='linear';
 nInputs = 30;
+nOutputs = 10;
+nFeatures = size(X,2);
+bias = 1;
 nHidden    = 1:50:1000;
-trainR2   = zeros(size(nHidden));
-testR2   = zeros(size(nHidden));
+trainMSE   = zeros(size(nHidden));
+testMSE   = zeros(size(nHidden));
 trainTimes = 1:numel(nHidden);
 for i = 1 : numel(nHidden)
-    ELM = ELM_Class(nInputs,nHidden(i),6,actFun,1);
+    ELM = ELM_Class(nInputs,nHidden(i),nOutputs,nFeatures,actFun,bias);
+    %rearranging and setting proportions could be moved above for loop and
+    %would be faster, but it would require creating ELM beforehand as
+    %rearrangedata function is method from ELM_Class.
+    [oX,oY]=rearrangeData(ELM,X);
+    [Xtest,Ytest,Xtrain,Ytrain] = setProportionsOfData(oX,oY,80);
     % train ELM on the training dataset
     tic;
     ELM = train(ELM,Xtrain,Ytrain);
     trainTimes(i)=toc;
     Yhat = predict(ELM,Xtrain);
-    trainR2(i) = ComputeR2(Ytrain(:,2:end-1),Yhat(:,2:end-1));
+    trainMSE(i) = immse(Ytrain,Yhat);
     % validation of ELM model
     Yhat = predict(ELM,Xtest);
-    testR2(i) = ComputeR2(Ytest(:,2:end-1),Yhat(:,2:end-1));
+    testMSE(i) = immse(Ytest,Yhat);
     
 end
-sprintf('Training time of %d networks with different number of Hidden Neurons :%0.5f',numel(nHidden),sum(trainTimes))
+sprintf('Training time(s) of %d networks with different number of Hidden Neurons :%0.5f',numel(nHidden),sum(trainTimes))
 % plot results
-plot(nHidden,[trainR2;testR2],'-o');
+plot(nHidden,[trainMSE;testMSE],'-o');
 xlabel('Number of Hidden Neurons');
-ylabel('R square error');
+ylabel('Mean square error');
 legend({'training','testing'},'Location','southeast')
+
+ figure
+ plot(1:size(trainTimes,2),trainTimes);
+ title('Training time(s) vs number of hidden neurons');
+ xlabel('Number of hidden neurons'); % x-axis label
+ ylabel('Training time');
